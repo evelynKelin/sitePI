8604337ff67de7d591423fe948880e398ee5e425
evelynKelin <112372@aluno.uricer.edu.br>
2025-08-16 00:37:38 -0300

    Primeiro commit do PI

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <nav className="menu">
+        <ul>
+          <li><a href="#">Início</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Serviços</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+
+export default App
