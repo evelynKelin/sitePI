14ce36b48696c41de2a9ea67259f2875bf15b29c
Evelyn Fabisiak <seu.email@exemplo.com>
2025-08-24 03:31:30 -0300

    Versao p1 pronta e p2 iniciada

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import Login from './login'; // importa a página de login
+import copo from './assets/img/copo.png'; 
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
           <li><Link to="/">logo loja</Link></li>
           <li><Link to="/">NomeLoja</Link></li>
           <li id="mDiferente">
-            <Link to="/login"><button>Login</button></Link>
+            <Link to="/login"><button className='botaoUniversal'>Login</button></Link>
           </li>
         </ul>
       </nav>
@@ -27,9 +28,46 @@ function App() {
 
 function Home() {
   return (
-    <div>
-      <h1>Algum dia vai ser um site</h1>
+
+    <div id="pai">
+
+<li><Link to="/p2">Página 2</Link></li>
+      <div id="filho1">
+      <h1 className="fontePrincipal">Modelo Copo</h1>
     </div>
+
+    <div id="filho2">
+      
+      <img src={copo} alt="Copo" />
+    </div>
+
+   <div class="anelFundo"></div>
+
+
+    <div id="filho3">
+    
+      <h2 className='fonteSecundaria'>Algumas informações sobre o copo</h2>
+      <p className="paragrafo">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit
+        Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit
+      </p>
+
+      </div>
+
+
+    <div id="filho4">
+       <div className='fundinho2'>
+      <h2 className='fonteSecundaria'>O desing</h2>
+      <p className="paragrafo">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit
+        Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit
+      </p>
+
+      </div>
+      </div>
+
+
+    </div>
+
+ 
   );
 }
 
