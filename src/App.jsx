ed4635aae198cff6577aea3171d7c01e169adb48
Evelyn Fabisiak <seu.email@exemplo.com>
2025-09-12 00:11:59 -0300

    modifiquei tudo precisa arrumar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,92 @@
-import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
-import Login from './login'; // importa a página de login
-import copo from './assets/img/copo.png'; 
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import copo from "./assets/img/copo.png";
+import "./App.css"; // CSS importado aqui
 
-function App() {
+// Componente de ícone (Seta)
+function Seta({ direcao = "arrow_back", onClick, className }) {
   return (
-    <div>
-      {/* Menu */}
+    <span
+      className={`material-symbols-outlined ${className}`}
+      onClick={onClick}
+    >
+      {direcao}
+    </span>
+  );
+}
+
+// Página Home
+const Home = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div id="pai">
+      <div id="filho1">
+        <h1 className="fontePrincipal">Modelo Copo</h1>
+      </div>
+
+      <div id="filho2">
+        <img src={copo} alt="Copo" />
+      </div>
+
+      <div className="anelFundo"></div>
+
+      <div id="filho3">
+        <h2 className="fonteSecundaria">Algumas informações sobre o copo</h2>
+        <p className="paragrafo">
+          Neque porro quisquam est qui dolorem ipsum quia dolor sit amet...
+        </p>
+      </div>
+
+      <div id="filho4">
+        <div className="fundinho2">
+          <h2 className="fonteSecundaria">O design</h2>
+          <p className="paragrafo">
+            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet...
+          </p>
+        </div>
+      </div>
+
+      {/* Seta direita para ir para Login */}
+      <Seta
+        direcao="arrow_forward"
+        onClick={() => navigate("/login")}
+        className="seta-direita"
+      />
+    </div>
+  );
+};
+
+// Página Login
+const Login = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div id="login">
+      <h1>Login</h1>
+      <p>Faça login aqui.</p>
+
+      {/* Seta esquerda para voltar para Home */}
+      <Seta
+        direcao="arrow_back"
+        onClick={() => navigate("/")}
+        className="seta-esquerda"
+      />
+    </div>
+  );
+};
+
+// App principal
+export default function App() {
+  return (
+    <>
+      {/* Menu global */}
       <nav className="menuzinho">
         <ul>
-          <li><Link to="/">logo loja</Link></li>
-          <li><Link to="/">NomeLoja</Link></li>
-          <li id="mDiferente">
-            <Link to="/login"><button className='botaoUniversal'>Login</button></Link>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
           </li>
         </ul>
       </nav>
@@ -22,53 +96,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-    </div>
+    </>
   );
 }
-
-function Home() {
-  return (
-
-    <div id="pai">
-
-<li><Link to="/p2">Página 2</Link></li>
-      <div id="filho1">
-      <h1 className="fontePrincipal">Modelo Copo</h1>
-    </div>
-
-    <div id="filho2">
-      
-      <img src={copo} alt="Copo" />
-    </div>
-
-   <div class="anelFundo"></div>
-
-
-    <div id="filho3">
-    
-      <h2 className='fonteSecundaria'>Algumas informações sobre o copo</h2>
-      <p className="paragrafo">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit
-        Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit
-      </p>
-
-      </div>
-
-
-    <div id="filho4">
-       <div className='fundinho2'>
-      <h2 className='fonteSecundaria'>O desing</h2>
-      <p className="paragrafo">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit
-        Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit
-      </p>
-
-      </div>
-      </div>
-
-
-    </div>
-
- 
-  );
-}
-
-export default App;
